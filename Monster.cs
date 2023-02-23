d7956d403dd60108a2346f924504d46e7307c01c
Kaixiang8 <1058106@hr.nl>
2023-02-23 12:05:48 +0100

    Added monster

---
+++ Monster.cs
@@ -0,0 +1,13 @@
+public class Monster
+{
+    public string Type;
+    public int Health;
+    public int Damage;
+
+    public Monster(string type, int health, int damage)
+    {
+        Type = type;
+        Health = health;
+        Damage = damage;
+    }
+}
