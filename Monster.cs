775d8a7b6623ae2b2f8c6ac0d8e6cddd21aa7d24
Kaixiang8 <1058106@hr.nl>
2023-02-23 12:07:42 +0100

    Change Monster - added name

--- Monster.cs
+++ Monster.cs
@@ -3,11 +3,13 @@ public class Monster
     public string Type;
     public int Health;
     public int Damage;
+    public string Name;
 
-    public Monster(string type, int health, int damage)
+    public Monster(string type, int health, int damage, string name)
     {
         Type = type;
         Health = health;
         Damage = damage;
+        Name = name;
     }
 }
